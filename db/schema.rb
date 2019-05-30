da3150afb7c49fdbd4ac4c323111eafd83e68e8b
Anna <Annak1794@gmail.com>
2019-05-30 21:31:09 +0300

    new schema generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_170617) do
+ActiveRecord::Schema.define(version: 2019_05_30_182137) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 2019_05_24_170617) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "joined_table_users_tests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
   end
 
   create_table "questions", force: :cascade do |t|
