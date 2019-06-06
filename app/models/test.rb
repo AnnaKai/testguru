2d92c71000f15c38db139c541f439ed54f47e4ee
Anna <Annak1794@gmail.com>
2019-06-06 16:25:44 +0300

    Association renamed

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, class_name: "User"
+
   has_many :questions
   has_many :test_passes
   has_many :users, through: :test_passes
