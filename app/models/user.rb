db3f610dd81d17d5a5079cd58cd967bca5537810
Anna <Annak1794@gmail.com>
2019-06-06 17:07:54 +0300

    Tiny fix for Author association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
   has_many :test_passes
   has_many :tests, through: :test_passes
+  has_many :created_tests, class_name: "Test"
 
   def tests_by_level(level)
     tests.where("level = ?", level)
