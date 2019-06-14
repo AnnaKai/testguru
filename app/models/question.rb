2123f6b0c4b5b0495cc0ea26e43d57633c7e6e98
Anna <Annak1794@gmail.com>
2019-06-15 00:00:55 +0300

    updated seed, questions controller, views for questions and all tests

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-  belongs_to :test
+  belongs_to :test, required: false
   has_many :answers
 
   validates :body, presence: true
--- app/models/test_pass.rb
+++ app/models/test_pass.rb
@@ -1,5 +1,5 @@
 class TestPass < ApplicationRecord
   self.table_name = "tests_users"
-  belongs_to :test
-  belongs_to :user
+  belongs_to :test, required: false
+  belongs_to :user, required: false
 end
