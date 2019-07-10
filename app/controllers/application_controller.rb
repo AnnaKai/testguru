f5e4b92bfc445b71efbc47790d766e0a4074aee7
Anna <Annak1794@gmail.com>
2019-07-10 03:39:56 +0300

    basic locale config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   def after_sign_in_path_for(resource)
     resource.admin? ? admin_tests_path : tests_path
+  end
+
+  def default_url_options
+    { lang: I18n.locale }
   end
 
   protected
@@ -11,6 +16,12 @@ class ApplicationController < ActionController::Base
     added_attrs = [:username, :first_name, :last_name]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 
 end
