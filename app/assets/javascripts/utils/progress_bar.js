3c39b481313dddc4aef79599dc1e2749ce845e93
Anna <Annak1794@gmail.com>
2019-08-20 21:08:12 +0300

    added if statement for progress bar

--- app/assets/javascripts/utils/progress_bar.js
+++ app/assets/javascripts/utils/progress_bar.js
@@ -1,6 +1,8 @@
 document.addEventListener('turbolinks:load', function () {
     var bar = document.querySelector('.progress-bar');
-    var percent = bar.dataset.percent;
-    var filled = document.querySelector('.progress-bar[data-percent="' + percent + '"]');
-    filled.style.width = percent + "%";
+    if (bar) {
+        var percent = bar.dataset.percent;
+        var filled = document.querySelector('.progress-bar[data-percent="' + percent + '"]');
+        filled.style.width = percent + "%";
+    }
 });
